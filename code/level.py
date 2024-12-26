be9abb833c83012de2da42cf633042069530ae3a
jamesperram92 <jamesperram@gmail.com>
2024-12-26 15:54:14 +1100

    Updated overlay in the overlay to embed this into the level file top left hand side for now

--- code/level.py
+++ code/level.py
@@ -22,3 +22,4 @@ class Level:
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
         self.all_sprites.update(dt)
+        self.overlay.display()
--- code/overlay.py
+++ code/overlay.py
@@ -9,9 +9,12 @@ class Overlay:
 
         # Use self to reference instance variables
         overlay_path = 'graphics/overlay/'
-        self.tools_surf = {tool: pygame.image.load(f'{overlay_path}{tool}.png').convert_alpha for tool in player.tools}
-        self.seeds_surf = {seed: pygame.image.load(f'{overlay_path}{seed}.png').convert_alpha for seed in player.seeds}
+        self.tools_surf = {tool: pygame.image.load(f'{overlay_path}{tool}.png').convert_alpha() for tool in player.tools}
+        self.seeds_surf = {seed: pygame.image.load(f'{overlay_path}{seed}.png').convert_alpha() for seed in player.seeds}
 
-        # Access instance variables correctly
-        print(self.tools_surf)
-        print(self.seeds_surf)
+    def display(self):
+        #tools
+        tool_surf = self.tools_surf[self.player.selected_tool]
+        self.display_surface.blit(tool_surf,(0,0))
+        #seed
+        #seed_surf = self.seeds_surf[self.player.selected_seed]
