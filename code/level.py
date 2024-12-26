a75c185a59dfa11896c6deed517d91c685ae322a
jamesperram92 <jamesperram@gmail.com>
2024-12-26 21:39:40 +1100

    Using tmx_data to import the map from tiled, this is used to import the fence and the house using layer logic

--- code/level.py
+++ code/level.py
@@ -4,6 +4,7 @@ from settings import *
 from player import Player
 from overlay import Overlay
 from sprites import Generic
+from pytmx.util_pygame import load_pygame
 
 class Level:
     def __init__(self):
@@ -18,6 +19,20 @@ class Level:
         self.overlay = Overlay(self.player)
     
     def setup(self):
+        tmx_data = load_pygame('data/map.tmx')
+
+        #house
+        for layer in ['HouseFloor', 'HouseFurnitureBottom']:
+            for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
+                Generic((x * TITLE_SIZE, y* TITLE_SIZE), surf, self.all_sprites, LAYERS['house bottom'])
+        
+        for layer in ['HouseWalls', 'HouseFurnitureTop']:
+                    for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
+                        Generic((x * TITLE_SIZE, y* TITLE_SIZE), surf, self.all_sprites, LAYERS['main'])
+        #fence
+        for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
+            Generic((x * TITLE_SIZE, y* TITLE_SIZE), surf, self.all_sprites, LAYERS['main'])
+
         Generic(pos = (0,0), 
                 surf = pygame.image.load('graphics/world/ground.png').convert_alpha(),
                 groups = self.all_sprites,
