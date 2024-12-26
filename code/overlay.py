2430b7e4c6d2ca0bfab94598dccf9d6441500c4e
jamesperram92 <jamesperram@gmail.com>
2024-12-26 16:04:45 +1100

    Overlay working with the tools and seeds showing the correct options and working

--- code/overlay.py
+++ code/overlay.py
@@ -15,6 +15,10 @@ class Overlay:
     def display(self):
         #tools
         tool_surf = self.tools_surf[self.player.selected_tool]
-        self.display_surface.blit(tool_surf,(0,0))
+        tool_rect = tool_surf.get_rect(midbottom = OVERLAY_POSITIONS['tool'])
+        self.display_surface.blit(tool_surf,tool_rect)
+
         #seed
-        #seed_surf = self.seeds_surf[self.player.selected_seed]
+        seed_surf = self.seeds_surf[self.player.selected_seed]
+        seed_rect = seed_surf.get_rect(midbottom = OVERLAY_POSITIONS['seed'])
+        self.display_surface.blit(seed_surf,seed_rect)
--- code/settings.py
+++ code/settings.py
@@ -3,3 +3,9 @@ from pygame.math import Vector2
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TITLE_SIZE = 64
+
+#overlay positions
+OVERLAY_POSITIONS = {
+    'tool': (40, SCREEN_HEIGHT - 15),
+    'seed': (70, SCREEN_HEIGHT - 5)
+}
