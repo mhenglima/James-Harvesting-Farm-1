c1a6e39ad95524ae47166867f49f9f142a5f972e
jamesperram92 <jamesperram@gmail.com>
2024-12-26 20:12:33 +1100

    Added the background behind the game - sprites have been updated via new python file to handle the import of the images

--- code/level.py
+++ code/level.py
@@ -1,7 +1,9 @@
 import pygame
+import pygame.camera
 from settings import *
 from player import Player
 from overlay import Overlay
+from sprites import Generic
 
 class Level:
     def __init__(self):
@@ -9,17 +11,32 @@ class Level:
         self.display_surface = pygame.display.get_surface() 
 
         #sprite groups (allte players, trees etc)
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = CameraGroup()
 
         self.setup()
         #create after the setup of the player
         self.overlay = Overlay(self.player)
     
     def setup(self):
+        Generic(pos = (0,0), 
+                surf = pygame.image.load('graphics/world/ground.png').convert_alpha(),
+                groups = self.all_sprites)
+        
         self.player = Player((640,360), self.all_sprites)
 
     def run(self,dt):
         self.display_surface.fill('black')
-        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.custom_draw()
         self.all_sprites.update(dt)
         self.overlay.display()
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self, *sprites):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        
+    def custom_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect)
+
+    
